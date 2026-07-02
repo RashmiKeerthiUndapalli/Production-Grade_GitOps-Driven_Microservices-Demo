d96a862b071f8b2f3dd0e7a9279ddcd6837c48e2
Rashmi <84272096+RashmiKeerthiUndapalli@users.noreply.github.com>
2026-07-02 20:55:48 +0530

    Add S3 backend configuration to terraform.tf
    Configure S3 backend for Terraform state management.

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "devopsdock-terraform-backend-bucket-456"
+    key    = "s3-backend"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
